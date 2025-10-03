2a3430315bafe0ad4b533f28993e6facf5290ba1
Robert Pławski <dev@robertplawski.pl>
2025-10-03 17:51:44 +0200

    Basic turso example

--- app/index.ts
+++ app/index.ts
@@ -1,37 +1,14 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
-import { usersTable } from './db/schema';
-async function main() {
-  console.log(process.env.TURSO_URL)
-  console.log(process.env.TURSO_TOKEN)
+
+export const getDb = async () => {
+
   const client = createClient({
     url: process.env.TURSO_URL!,
     authToken: process.env.TURSO_TOKEN!
   });
-  const db = drizzle({ client });
-  const user: typeof usersTable.$inferInsert = {
-    name: 'John',
-    age: 30,
-    email: 'john@example.com',
-  };
-
-  await db.insert(usersTable).values(user);
-  console.log('New user created!')
-
-  const users = await db.select().from(usersTable);
-  console.log('Getting all users from the database: ', users)
-
-  await db
-    .update(usersTable)
-    .set({
-      age: 31,
-    })
-    .where(eq(usersTable.email, user.email));
-  console.log('User info updated!')
-
-  await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  console.log('User deleted!')
+  return drizzle({ client });
 }
-main();
+
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
+import { getDb } from "./index";
+import { usersTable } from "./db/schema";
 
-export default function Home() {
+export default async function Home() {
+  "use server"
+  const db = await getDb()
   return (
+
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      {JSON.stringify(await db.select().from(usersTable))}
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
