68770d1e24d455cebd9342e722725ec6ffed5eea
Robert Pławski <dev@robertplawski.pl>
2025-10-03 17:38:38 +0200

    Added turso integration

---
+++ app/index.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
+
+const client = createClient({
+  url: process.env.TURSO_URL!,
+  authToken: process.env.TURSO_TOKEN!
+});
+const db = drizzle({ client });
+
---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './app/db/schema.ts',
+  dialect: 'turso',
+  dbCredentials: {
+    url: process.env.TURSO_URL!,
+    authToken: process.env.TURSO_TOKEN!,
+  },
+});
+
