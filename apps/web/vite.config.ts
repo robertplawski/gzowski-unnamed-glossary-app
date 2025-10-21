34e6ba05030a457df1cc2bd41bda935ec7ac404a
Robert Pławski <dev@robertplawski.pl>
2025-10-21 16:21:43 +0200

    initial commit

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,25 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	plugins: [tailwindcss(), tanstackRouter({}), react(), VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "gzowski-unnamed-glossary-app",
+        short_name: "gzowski-unnamed-glossary-app",
+        description: "gzowski-unnamed-glossary-app - PWA Application",
+        theme_color: "#0c0c0c",
+      },
+      pwaAssets: { disabled: false, config: true },
+      devOptions: { enabled: true },
+    })],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
