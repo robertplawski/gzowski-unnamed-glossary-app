fbded6a113ee422fbc5f0b889cfacd7962e9351b
PanWor <dawideksudnicki@gmail.com>
2025-10-22 20:52:18 +0200

    refactor: changed in a home component: quit -> stop, single quotation marks; added nbsp

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import {createFileRoute} from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
 	component: HomeComponent,
@@ -9,7 +9,7 @@ interface TooltipAbbrProps {
 	definition: string;
 }
 
-const TooltipAbbr = ({ abbr, definition }: TooltipAbbrProps) => {
+const TooltipAbbr = ({abbr, definition}: TooltipAbbrProps) => {
 	return (
 		<span className="group text-primary relative inline-block">
 			<abbr className="border-b-8 border-dotted border-foreground no-underline cursor-help">
@@ -27,22 +27,22 @@ function HomeComponent() {
 		<div className="max-w-6xl mx-auto container">
 			<div className="grid gap-8 p-8 sm:p-12 md:p-24">
 				<h1 className="font-bold text-6xl sm:text-7xl md:text-8xl leading-20 sm:leading-26 md:leading-32">
-					Quit{" "}
+					Stop{" "}
 					<span className="group relative inline-block">
 						<TooltipAbbr
-							abbr=' "babbling" '
+							abbr=" ‘babbling’ "
 							definition="verb. to speak without clarity, like an infant e.g. gaga guga"
 						/>
 					</span>{" "}
-					in English with{" "}
+					in&nbsp;English with{" "}
 					<TooltipAbbr
 						abbr="GUGA"
 						definition="abbr. Gzowski unnamed glossary app"
 					/>
 				</h1>
 				<h2 className="text-2xl sm:text-3xl md:text-4xl leading-8 sm:leading-10 md:leading-16">
-					Transform your vocabulary with GUGA - app that makes remembering words
-					effortless!
+					Transform your vocabulary with GUGA – an app that makes remembering
+					words effortless!
 				</h2>
 			</div>
 		</div>
