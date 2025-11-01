b3ea1369ae2ea36f4ef2f8d59598f2c8d3fffa9a
Robert Pławski <dev@robertplawski.pl>
2025-11-01 23:24:32 +0100

    chore: fighting with cf branch deploys

--- apps/web/src/utils/getServerUrl.ts
+++ apps/web/src/utils/getServerUrl.ts
@@ -1,3 +1,5 @@
 export default function getServerUrl() {
-	return import.meta.env.VITE_SERVER_URL;
+	return import.meta.env.VITE_SERVER_URL
+		? import.meta.env.VITE_SERVER_URL
+		: `https://${import.meta.env.WORKERS_CI_BRANCH}-gzowski-unnamed-glossary-app-server.rp8.workers.dev`;
 }
