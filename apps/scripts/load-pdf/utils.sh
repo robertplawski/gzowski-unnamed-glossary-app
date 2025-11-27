321c0e6217c8f3ce0d3b455ecc45ef7662ad7190
Robert Pławski <dev@robertplawski.pl>
2025-11-27 21:13:37 +0100

    import: changed the utils shell script to work with focus 3 pdf

--- apps/scripts/load-pdf/utils.sh
+++ apps/scripts/load-pdf/utils.sh
@@ -1,7 +1,14 @@
+source .venv/bin/activate
 # load pages into txt
-python load-pdf.py split assets/focus-4.pdf --output-dir assets/split --pages 16,17,32,33,48,49,64,65,80,81,96,97,112,113,128,129
+python load-pdf.py split assets/focus-4.pdf --output-dir assets/split/focus-4 --pages 16,17,32,33,48,49,64,65,80,81,96,97,112,113,128,129
 # read pages
-python load-pdf.py read-pages assets/split 16,17,32,33,48,49,64,65,80,81,96,97,112,113,128,129
-
+python load-pdf.py read-pages assets/split/focus-4 16,17,32,33,48,49,64,65,80,81,96,97,112,113,128,129
 # test regex
-python load-pdf.py test-regex assets/split 16,17,32,33,48,49,64,65,80,81,96,97,112,113,128,129 -o assets/output.json
+python load-pdf.py test-regex assets/split/focus-4 16,17,32,33,48,49,64,65,80,81,96,97,112,113,128,129 -o assets/focus-4.json
+
+# load pages into txt
+python load-pdf.py split assets/focus-3.pdf --output-dir assets/split/focus-3 --pages 15,16,29,43,57,71,85,99,113
+# read pages
+python load-pdf.py read-pages assets/split/focus-3 15,16,29,43,57,71,85,99,113
+# test regex
+python load-pdf.py test-regex assets/split/focus-3 15,16,29,43,57,71,85,99,113 -o assets/focus-3.json
