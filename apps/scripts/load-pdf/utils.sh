8df7dad907ff02b86f848dc98f15b12ec1e8de02
Robert Pławski <dev@robertplawski.pl>
2025-11-27 21:46:59 +0100

    import: updated the utils.sh to work with focus 1 pdf

--- apps/scripts/load-pdf/utils.sh
+++ apps/scripts/load-pdf/utils.sh
@@ -1,4 +1,12 @@
 source .venv/bin/activate
+
+# load pages into txt
+python load-pdf.py split assets/focus-1.pdf --output-dir assets/split/focus-1 --pages 21,33,45,57,69,81,93,105
+# read pages
+python load-pdf.py read-pages assets/split/focus-1 21,33,45,57,69,81,93,105
+# test regex
+python load-pdf.py test-regex assets/split/focus-1 21,33,45,57,69,81,93,105 -o assets/focus-1.json
+
 # load pages into txt
 python load-pdf.py split assets/focus-4.pdf --output-dir assets/split/focus-4 --pages 16,17,32,33,48,49,64,65,80,81,96,97,112,113,128,129
 # read pages
