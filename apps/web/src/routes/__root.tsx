6261bdb00b2464c7957c803c05a23b4743043435
Robert Pławski <dev@robertplawski.pl>
2026-02-02 16:39:38 +0100

    fix: metadata fixes, changed favicon and sites title

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -10,10 +10,10 @@ import { createTanstackQueryUtils } from "@orpc/tanstack-query";
 import type { AppRouterClient } from "@gzowski-unnamed-glossary-app/api/routers/index";
 import { createORPCClient } from "@orpc/client";
 import {
-	HeadContent,
-	Outlet,
-	createRootRouteWithContext,
-	useRouterState,
+  HeadContent,
+  Outlet,
+  createRootRouteWithContext,
+  useRouterState,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import "../index.css";
@@ -21,73 +21,74 @@ import Footer from "@/components/footer";
 import MobileBottomNav from "@/components/mobile-bottom-nav";
 
 export interface RouterAppContext {
-	orpc: typeof orpc;
-	queryClient: QueryClient;
+  orpc: typeof orpc;
+  queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<RouterAppContext>()({
-	component: RootComponent,
-	notFoundComponent: NotFoundComponent,
-	head: () => ({
-		meta: [
-			{
-				title: "gzowski-unnamed-glossary-app",
-			},
-			{
-				name: "description",
-				content: "gzowski-unnamed-glossary-app is a web application",
-			},
-		],
-		links: [
-			{
-				rel: "icon",
-				href: "/favicon.ico",
-			},
-		],
-	}),
+  component: RootComponent,
+  notFoundComponent: NotFoundComponent,
+  head: () => ({
+    meta: [
+      {
+        title: "Gzowski unnamed glossary app",
+      },
+      {
+        name: "description",
+        content:
+          "Transform your vocabulary with GUGA – an app that makes remembering words effortless!",
+      },
+    ],
+    links: [
+      {
+        rel: "icon",
+        href: "/favicon.ico",
+      },
+    ],
+  }),
 });
 
 function NotFoundComponent() {
-	return (
-		<div className="max-w-6xl mx-auto container p-4">
-			<div className="grid gap-6 p-4 sm:p-8 md:p-24">
-				<h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-				<p className="text-lg">
-					Sorry, the page you're looking for doesn't exist.
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="max-w-6xl mx-auto container p-4">
+      <div className="grid gap-6 p-4 sm:p-8 md:p-24">
+        <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+        <p className="text-lg">
+          Sorry, the page you're looking for doesn't exist.
+        </p>
+      </div>
+    </div>
+  );
 }
 function RootComponent() {
-	const isFetching = useRouterState({
-		select: (s) => s.isLoading,
-	});
+  const isFetching = useRouterState({
+    select: (s) => s.isLoading,
+  });
 
-	const [client] = useState<AppRouterClient>(() => createORPCClient(link));
-	const [orpcUtils] = useState(() => createTanstackQueryUtils(client));
+  const [client] = useState<AppRouterClient>(() => createORPCClient(link));
+  const [orpcUtils] = useState(() => createTanstackQueryUtils(client));
 
-	return (
-		<>
-			<HeadContent />
-			<ThemeProvider
-				attribute="class"
-				defaultTheme="dark"
-				disableTransitionOnChange
-				storageKey="vite-ui-theme"
-			>
-				<div className="grid grid-rows-[auto_1fr_auto] min-h-svh  ">
-					<Header />
-					<div className="pb-20 md:pb-0">
-						{isFetching ? <Loader /> : <Outlet />}
-					</div>
-					<Footer />
-					<MobileBottomNav />
-				</div>
-				<Toaster richColors />
-			</ThemeProvider>
-			<TanStackRouterDevtools position="bottom-left" />
-			<ReactQueryDevtools position="bottom" buttonPosition="bottom-right" />
-		</>
-	);
+  return (
+    <>
+      <HeadContent />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        disableTransitionOnChange
+        storageKey="vite-ui-theme"
+      >
+        <div className="grid grid-rows-[auto_1fr_auto] min-h-svh  ">
+          <Header />
+          <div className="pb-20 md:pb-0">
+            {isFetching ? <Loader /> : <Outlet />}
+          </div>
+          <Footer />
+          <MobileBottomNav />
+        </div>
+        <Toaster richColors />
+      </ThemeProvider>
+      <TanStackRouterDevtools position="bottom-left" />
+      <ReactQueryDevtools position="bottom" buttonPosition="bottom-right" />
+    </>
+  );
 }
